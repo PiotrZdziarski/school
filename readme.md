8defe43ae62eb73e270e276f596fd8ba09a00453
Piotr <pzdziarski01@gmail.com>
2018-08-12 16:48:01 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# School webiste
+
+Technologies: Lumen, Bootstrap, CSS, HTML, JavaScript
+
+School: 1LO im. Józefa Chełmońskiego w Łowiczu 
+
+Previous website: http://lo1lowicz.pl/
---
+++ resources/views/components/claim.blade.php
@@ -0,0 +1,18 @@
+<section class="height-90vh py-5 flex-center jarallax" data-dark-overlay="4" style="background:url(assets/img/1920x800/main.jpg) no-repeat">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9 text-white">
+                <h2 class="display-lg-4 font-weight-bold text-primary wow slideInUp">
+                    Best Ever Trendy
+                </h2>
+                <h1 class="display-lg-3 font-weight-bold text-white wow slideInUp">
+                    School Template
+                </h1>
+                <p class="lead wow slideInUp">
+                    This modern and inviting academic template is perfectly suited for school, colleges, university, on-line course, and other educational institutions.
+                </p>
+                <a href="#" class="btn btn-primary btn-lg mt-3 wow slideInUp">Read More</a>
+            </div>
+        </div>
+    </div>
+</section>
