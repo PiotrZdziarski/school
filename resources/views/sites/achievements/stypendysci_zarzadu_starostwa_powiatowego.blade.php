e1ea4c6251aea697c2d6174f0cdd937f4904443e
Piotr <pzdziarski01@gmail.com>
2018-08-19 11:17:19 +0200

    stypendysci

---
+++ resources/views/sites/achievements/stypendysci_zarzadu_starostwa_powiatowego.blade.php
@@ -0,0 +1,156 @@
+@extends('layouts.app')
+@section('title')
+    Stypendyści Zarządu Starostwa Powiatowego w Łowiczu
+@endsection
+@section('content')
+
+
+    <div class="padding-y-60 bg-cover" data-dark-overlay="6"
+         style="background:url(assets/img/breadcrumb-bg.jpg) no-repeat; background-position: 55%;">
+        <div class="container">
+            <h1 class="text-white wow slideInLeft">
+                Stypendyści Zarządu Starostwa Powiatowego w <span style="font-family: Arial,sans-serif">Ł</span>owiczu
+            </h1>
+            <ol class="breadcrumb breadcrumb-double-angle text-white bg-transparent p-0 wow slideInLeft"
+                data-wow-delay=".3s">
+                <li class="breadcrumb-item"><a href="{{url('/')}}">Strona główna</a>
+                <li class="breadcrumb-item"><span class="opacity-08">Osiągnięcia</span>
+                <li class="breadcrumb-item"><span class="opacity-08">Stypendyści</span>
+                <li class="breadcrumb-item">Stypendyści Zarządu Starostwa Powiatowego w <span style="font-family: Arial,sans-serif">Ł</span>owiczu</li>
+            </ol>
+        </div>
+    </div>
+
+    <section class="padding-y-40 bg-light">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-8 mt-4 nopaddinglowwidth">
+                    <div class="card shadow -v1">
+                        <div class="card-body" style="padding: 0;">
+                            <section class="border-bottom border-light" id="content">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-12 mb-1 text-center paddingTop-35">
+                                            <h2>Stypendyści Zarządu Starostwa Powiatowego w <span style="font-family: Arial,sans-serif">Ł</span>owiczu</h2>
+                                        </div>
+                                        <div class="col-lg-12 mx-auto paddingBottom-40 padding20lowwidth padding-30 paddingTop-5" style="margin-top: -5px;">
+
+                                            <h6 class="text-center paddingTop-10">Za bardzo dobre wyniki w nauce, wzorowe zachowanie oraz aktywny udział w pracach na rzecz szkoły</h6>
+
+
+                                            <h5 class="paddingTop-50 text-center paddingBottom-10">Rok szkolny 2017/2018</h5>
+                                            <p>Michalina Wielemborek – 2b</p>
+                                            <p>Weronika Pierścieniewska – 2a</p>
+                                            <p>Szymon Salamondra – 2a</p>
+                                            <p>Marianna Zygmunt – 3a</p>
+                                            <p>Monika Brytkowska – 2a</p>
+                                            <p>Agnieszka Szachogłuchowicz – 2a</p>
+                                            <p>Karolina Dubiel – 3a</p>
+                                            <p>Karolina Jóźwiak – 2a</p>
+                                            <p>Hubert Kępka – 2b</p>
+                                            <p>Michał Wójcik – 2a</p>
+                                            <p>Oliwia Jankowska – 2a</p>
+                                            <p>Natalia Kowalska – 2c</p>
+                                            <p>Sara Traczyk – 2b</p>
+                                            <p>Paulina Golis – 3a</p>
+                                            <p>Magdalena Kapuścińska – 3a</p>
+                                            <p>Paulina Sieczkowska – 3a</p>
+                                            <p>Wioleta Siekiera – 3a</p>
+                                            <p>Szymon Gajewski – 2b</p>
+                                            <p>Karolina Kowalczyk – 2c</p>
+                                            <p>Katarzyna Zdziarska – 2b</p>
+                                            <p>Oliwia Karcz – 2c</p>
+                                            <p>Magdalena Laska – 2c</p>
+                                            <p>Ewelina Radaszkiewicz – 2a</p>
+                                            <p>Katarzyna Śmigielska – 2a</p>
+
+
+                                            <h5 class="paddingTop-50 text-center paddingBottom-10">Rok szkolny 2016/2017</h5>
+                                            <p>JAGODA KATARZYNA – 3b</p>
+                                            <p>KOWALCZYK WIKTORIA – 3a</p>
+                                            <p>ZACHACZ PAULA – 3a</p>
+                                            <p>KAPUŚCIŃSKA MAGDALENA – 2a</p>
+                                            <p>SIEKIERA WIOLETA – 2a</p>
+                                            <p>PIENIĄŻEK PAWEŁ – 3d</p>
+                                            <p>ZYGMUNT MARIANNA – 2a</p>
+                                            <p>STASIAK PAULINA – 3a</p>
+                                            <p>GOLIS PAULINA – 2a</p>
+                                            <p>DĘBSKI WOJCIECH – 2b</p>
+                                            <p>KUŚMIEREK DANIEL – 3c</p>
+                                            <p>FLORCZAK ANNA – 2a</p>
+                                            <p>PERZYNA NATALIA – 2a</p>
+                                            <p>NOWIŃSKA MARTYNA – 3a</p>
+
+
+                                            <h5 class="paddingTop-50 text-center paddingBottom-10">Rok szkolny 2015/2016</h5>
+                                            <p>AGATA BĄBA – 3e</p>
+                                            <p>MAJA GABRYSIAK – 3a</p>
+                                            <p>WERONIKA GRZEGORY – 3a</p>
+                                            <p>KATARZYNA JAGODA – 2b</p>
+                                            <p>ALEKSANDRA JAKUBIK– 3e</p>
+                                            <p>EWELINA KAMIŃSKA – 3b</p>
+                                            <p>WIKTORIA KOLEC – 3b</p>
+                                            <p>WIKTORIA KOWALCZYK – 2a</p>
+                                            <p>WOJCIECH KUREK – 3a</p>
+                                            <p>ANNA MARKOWSKA – 3b</p>
+                                            <p>KLAUDIA MILCZAREK – 3e</p>
+                                            <p>MARIA PLOCH – 3a</p>
+                                            <p>KATARZYNA PRYK – 3b</p>
+                                            <p>ALEKSANDRA ROŻNIATA – 3b</p>
+                                            <p>ANNA TOMASZKIEWICZ – 3a</p>
+                                            <p>DAWID WIELEMBOREK – 3a</p>
+                                            <p>JUSTYNA WÓJCIK – 3d</p>
+                                            <p>PAULA ZACHACZ – 2a</p>
+                                            <p>KATARZYNA ZNAJDEK – 3d</p>
+
+
+                                            <h5 class="paddingTop-50 text-center paddingBottom-10">Rok szkolny 2014/2015</h5>
+                                            <p>WERONIKA GRZEGORY – 2b</p>
+                                            <p>JAKUB KRYSICKI – 3b</p>
+                                            <p>ANNA MARKOWSKA – 2b</p>
+                                            <p>ALEKSANDRA ROŻNIATA – 2b</p>
+                                            <p>JOANNA RUDNICKA – 3d</p>
+
+                                            <h5 class="paddingTop-50 text-center paddingBottom-10">Rok szkolny 2013/2014</h5>
+                                            <p>DARIUSZ DUTKIEWICZ – 3e</p>
+                                            <p>JULIA GOLIS – 3b</p>
+                                            <p>JAKUB KRYSICKI – 2b</p>
+                                            <p>ANNA RADASZKIEWICZ – 2b</p>
+                                            <p>KLAUDIA SIEROTA – 3c</p>
+                                            <p>MATEUSZ TONDERSKI – 2c</p>
+
+
+                                            <h5 class="paddingTop-50 text-center paddingBottom-10">Rok szkolny 2012/2013</h5>
+                                            <p>JULIA GOLIS – 2b</p>
+                                            <p>EWA JABŁOŃSKA – 3b</p>
+                                            <p>JOANNA WILCZYŃSKA – 3b</p>
+
+                                            <h5 class="paddingTop-50 text-center paddingBottom-10">Rok szkolny 2011/2012</h5>
+                                            <p>JAROSŁAW GOLIS – 2c</p>
+                                            <p>MARTA NOWAK – 3b</p>
+                                            <p>KATARZYNA WIĘCŁAW – 3e</p>
+
+                                            <h5 class="paddingTop-50 text-center paddingBottom-30">Rok szkolny 2010/2011</h5>
+                                            <p>MARTA NOWAK – 2b</p>
+                                            <p>PAULINA TALAROWSKA – 2c</p>
+
+                                            <h5 class="paddingTop-50 text-center paddingBottom-10">Rok szkolny 2009/2010</h5>
+                                            <p>MATEUSZ TALAROWSKI – 3b</p>
+                                            <p>AGNIESZKA SZMAJDZIŃSKA – 3f</p>
+                                            <p>KATARZYNA ŻEMŁO – 3d</p>
+                                        </div> <!-- END row-->
+                                    </div> <!-- END container-->
+                                </div>
+                            </section> <!-- END section-->
+                            <!-- END tab-content-->
+                        </div>
+                    </div>
+                </div>
+
+                @include('components.aktualnosci')
+            </div> <!-- END row-->
+        </div> <!-- END container-->
+    </section>
+
+@endsection
