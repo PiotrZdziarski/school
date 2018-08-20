84b776d89e162b522ca349aebab584a699d10042
Piotr <pzdziarski01@gmail.com>
2018-08-20 21:15:41 +0200

    Adding rest of rekrtuacja subpages

--- app/Http/Controllers/RekrutacjaController.php
+++ app/Http/Controllers/RekrutacjaController.php
@@ -15,4 +15,24 @@ class RekrutacjaController extends Controller
     {
         return view('sites.rekrutacja.rekrutacja');
     }
+
+    public function rekrutacja1A()
+    {
+        return view('sites.rekrutacja.rekrutacja1A');
+    }
+
+    public function rekrutacja1B()
+    {
+        return view('sites.rekrutacja.rekrutacja1B');
+    }
+
+    public function rekrutacja1C()
+    {
+        return view('sites.rekrutacja.rekrutacja1C');
+    }
+
+    public function rekrutacja1D()
+    {
+        return view('sites.rekrutacja.rekrutacja1D');
+    }
 }
